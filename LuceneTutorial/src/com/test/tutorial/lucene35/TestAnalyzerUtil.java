1f95483c5b87dc98d29206e654fb911bd9092315
onyas <zheng0518@163.com>
2014-04-13 21:03:55 +0800

    显示分词的分有信息，包括起始位置，偏移量，类型

--- LuceneTutorial/src/com/test/tutorial/lucene35/TestAnalyzerUtil.java
+++ LuceneTutorial/src/com/test/tutorial/lucene35/TestAnalyzerUtil.java
@@ -27,4 +27,19 @@ public class TestAnalyzerUtil {
 		AnalyzerUtil.displayToken(str, a4);
 	}
 	
+	
+	@Test
+	public void testDisplayAllToken(){
+		Analyzer a1 = new StandardAnalyzer(Version.LUCENE_35);
+		Analyzer a2 = new StopAnalyzer(Version.LUCENE_35);
+		Analyzer a3 = new SimpleAnalyzer(Version.LUCENE_35);
+		Analyzer a4 = new WhitespaceAnalyzer(Version.LUCENE_35);
+		
+		String str = "hello this is just a test";
+//		String str ="这只是一个测试";
+		AnalyzerUtil.displayAllToken(str, a1);
+		AnalyzerUtil.displayAllToken(str, a2);
+		AnalyzerUtil.displayAllToken(str, a3);
+		AnalyzerUtil.displayAllToken(str, a4);
+	}
 }
--- LuceneTutorial/src/com/tutorial/lucene35/AnalyzerUtil.java
+++ LuceneTutorial/src/com/tutorial/lucene35/AnalyzerUtil.java
@@ -6,17 +6,43 @@ import java.io.StringReader;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
 
 public class AnalyzerUtil {
 
 	public static void displayToken(String str, Analyzer analyzer) {
 		try {
-			TokenStream stream = analyzer.tokenStream("content", new StringReader(
-					str));
-			//创建一个属性，这个属性会添加流中，随着这个TokenStream增加
-			CharTermAttribute cta = stream.addAttribute(CharTermAttribute.class);
-			while(stream.incrementToken()){
-				System.out.print("["+cta+"]");
+			TokenStream stream = analyzer.tokenStream("content",
+					new StringReader(str));
+			// 创建一个属性，这个属性会添加流中，随着这个TokenStream增加
+			CharTermAttribute cta = stream
+					.addAttribute(CharTermAttribute.class);
+			while (stream.incrementToken()) {
+				System.out.print("[" + cta + "]");
+			}
+			System.out.println();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void displayAllToken(String str, Analyzer analyzer) {
+		try {
+			TokenStream stream = analyzer.tokenStream("content",
+					new StringReader(str));
+			// 创建一个属性，这个属性会添加流中，随着这个TokenStream增加
+			CharTermAttribute cta = stream
+					.addAttribute(CharTermAttribute.class);
+			PositionIncrementAttribute pia = stream
+					.addAttribute(PositionIncrementAttribute.class);
+			OffsetAttribute oa = stream.addAttribute(OffsetAttribute.class);
+			TypeAttribute ta = stream.addAttribute(TypeAttribute.class);
+			while (stream.incrementToken()) {
+				System.out.print("position:" + pia.getPositionIncrement());
+				System.out.println(cta+"["+oa.startOffset() + "--" + oa.endOffset()
+						+ "]" + ta.type());
 			}
 			System.out.println();
 		} catch (IOException e) {
