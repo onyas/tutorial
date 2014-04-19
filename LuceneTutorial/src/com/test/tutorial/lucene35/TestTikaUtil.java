575929fcabecdcdf2b27549d0ab85171733333f8
onyas <zheng0518@163.com>
2014-04-19 12:59:11 +0800

    Tika简单使用，读取word文档

---
+++ LuceneTutorial/src/com/test/tutorial/lucene35/TestTikaUtil.java
@@ -0,0 +1,23 @@
+package com.test.tutorial.lucene35;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.tutorial.lucene35.TikaUtil;
+
+public class TestTikaUtil {
+
+	@Test
+	public void index() {
+		TikaUtil tu = new TikaUtil();
+		tu.index();
+	}
+	
+	@Test
+	public void testFile2Text(){
+		TikaUtil tu = new TikaUtil();
+		System.out.println(tu.file2Text(new File("F:\\Test\\lucene\\tikaexample\\招聘试题.doc")));
+	}
+
+}
