7746398c8737496519486242a8177b478ec6185f
onyas <zheng0518@163.com>
2014-04-24 21:59:48 +0800

    在配置文件里引入mysql数据库连接的属性文件

--- Spring4.0Tutorial/src/com/test/spring4/TestDataSource.java
+++ Spring4.0Tutorial/src/com/test/spring4/TestDataSource.java
@@ -1,5 +1,7 @@
 package com.test.spring4;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,12 +11,15 @@ import com.tutorial.spring4.DataSource;
 public class TestDataSource {
 
 	@Test
-	public void test() {
+	public void test() throws SQLException {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
 		
 		DataSource ds = (DataSource) ctx.getBean("dataSource");
 		System.out.println(ds);
+		
+		javax.sql.DataSource dataSource = (javax.sql.DataSource) ctx.getBean("dataSource2");
+		System.out.println(dataSource.getConnection());
 	}
 
 }
